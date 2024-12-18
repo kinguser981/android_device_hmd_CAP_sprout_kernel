62f348ecf517c2d34c959db11898e717faf655b4
Samuel Kendall <kinguser981@gmail.com>
2024-12-18 18:37:19 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 make clean && make mrproper
-make CONFIG=nokia_cap_spout_defconfig
-ARCH=arm64
+make CONFIG=nokia_cap_spout_defconfig ARCH=arm64 SUBARCH=arm64
 PATH="/workspace/Empty/clang/bin:/workspace/Empty/toolchains-for-snapdragon/aarch64-linux-android-4.9-master/aarch64-linux-android/bin:${PATH}" \
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- -C $(pwd)
