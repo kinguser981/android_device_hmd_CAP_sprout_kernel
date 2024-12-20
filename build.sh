eb9ab70c15e9b1ac237d104738f3a9e2ff4322c7
Samuel Kendall <kinguser981@gmail.com>
2024-12-20 03:59:30 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,3 +17,20 @@ export ARCH=arm64
 make O=out capspout_defconfig
 
 make -j$(nproc --all) O=out CC=/workspace/Empty/toolchains/clang-r428724/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=/workspace/Empty/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+
+
+
+
+
+
+
+
+make clean && make mrproper
+export ARCH=arm64
+make O=out capspout_defconfig
+make -j$(nproc --all) O=out CC=/workspace/Empty/toolchains/clang-r428724/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=/workspace/Empty/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+
+
+
