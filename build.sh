a22d4d25e53bfcf6830440cc8d62286f24f10328
Samuel Kendall <kinguser981@gmail.com>
2024-12-20 03:42:00 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 make clean && make mrproper
-make CONFIG=capspout_defconfig ARCH=arm64 SUBARCH=arm64
+export ARCH=arm64
+export SUBARCH=arm64
+export CROSS_COMPILE=aarch64-linux-android-
+export CLANG_TRIPLE=aarch64-linux-gnu-
+make capspout_defconfig CROSS_COMPILE=aarch64-linux-android-
 PATH="/workspace/Empty/clang/bin:/workspace/Empty/toolchains-for-snapdragon/aarch64-linux-android-4.9-master/aarch64-linux-android/bin:${PATH}" \
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- -C $(pwd)
+make -j$(nproc --all) O=out CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- -C $(pwd)
